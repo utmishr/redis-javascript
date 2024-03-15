11824847b0f149cfbd7ed15706f61467d8a5aedb
Utkarsh <ut2002mishra@gmail.com>
2024-03-15 23:34:39 +0530

    pass 5-3 stage

--- app/main.js
+++ app/main.js
@@ -1,19 +1,17 @@
 const net = require("net");
 const RequestParser = require("./RequestParser");
 
-// Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-  // Handle connection
-  //Implementing simple Ping -- will handle multiple ping from single client
   connection.on("data", (data) => {
     const request = data.toString().trim();
     const parser = new RequestParser(request);
     const command = parser.parse();
 
-    if (command.length > 0 && command[0] === "ECHO") {
+    if (command.length > 0 && command[0].toUpperCase() === "ECHO") {
       // If the command is ECHO, echo back the argument
-      const echoedString = command[1]; // Assuming the echoed string is the second argument
-      connection.write(`$${echoedString.length}\r\n${echoedString}\r\n`);
+      const echoedString = command[1];
+      const response = `$${echoedString.length}\r\n${echoedString}\r\n`;
+      connection.write(response);
     } else {
       // If the command is not recognized or not implemented, return "PONG"
       connection.write(`+PONG\r\n`);
