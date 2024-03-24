41e27ee71120f3c90508624dfffae3e19f6ad191
Utkarsh <ut2002mishra@gmail.com>
2024-03-24 07:16:49 +0530

    pass 17 stage

--- app/MasterServer.js
+++ app/MasterServer.js
@@ -68,8 +68,7 @@ class MasterServer {
         socket.write(this.handleReplconf(args.slice(1)));
         break;
       case "psync":
-        socket.write(this.handlePsync(args.slice(1)));
-        1;
+        socket.write(this.handlePsync(args.slice(1), socket));
         break;
     }
   }
@@ -113,10 +112,21 @@ class MasterServer {
   handleReplconf(args) {
     return Encoder.createSimpleString("OK");
   }
-  handlePsync(args) {
-    return Encoder.createSimpleString(
-      `FULLRESYNC ${this.masterReplId} ${this.masterReplOffset}`
+  handlePsync(args, socket) {
+    socket.write(
+      Encoder.createSimpleString(
+        `FULLRESYNC ${this.masterReplId} ${this.masterReplOffset}`
+      )
     );
+    const emptyRDB =
+      "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2";
+    const buffer = Buffer.from(emptyRDB, "hex");
+    const finalBuffer = Buffer.concat([
+      Buffer.from(`$${buffer.length}\r\n`),
+      buffer,
+    ]);
+    1;
+    return finalBuffer;
   }
 }
 module.exports = MasterServer;
